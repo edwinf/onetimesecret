31868d21740c45e9805a6ccb604e590b72233d9c
Ed Frey <edwinf@users.noreply.github.com>
2019-11-03 16:36:33 -0500

    add additional fields to match 2.0 status object data

--- src/OneTimeSecret.Web/Services/HealthChecks/HealthCheckResponseWriter.cs
+++ src/OneTimeSecret.Web/Services/HealthChecks/HealthCheckResponseWriter.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using OneTimeSecret.Web.Utiltiies;
 
 namespace OneTimeSecret.Web.Services.HealthChecks
 {
@@ -20,10 +23,24 @@ namespace OneTimeSecret.Web.Services.HealthChecks
                     new JProperty(pair.Key, new JObject(
                         new JProperty("status", pair.Value.Status.ToString()),
                         new JProperty("description", pair.Value.Description),
+                        new JProperty("responseTime", pair.Value.Duration.TotalMilliseconds),
+                        new JProperty("exception", FilterExceptionIfPublicCall(httpContext, pair)),
                         new JProperty("data", new JObject(pair.Value.Data.Select(
                             p => new JProperty(p.Key, p.Value))))))))));
             return httpContext.Response.WriteAsync(
                 json.ToString(Formatting.Indented));
+        }
+
+        private static object FilterExceptionIfPublicCall(HttpContext httpContext, KeyValuePair<string, HealthReportEntry> pair)
+        {
+            if (httpContext.Connection.RemoteIpAddress.IsInternal())
+            {
+                return JsonConvert.SerializeObject(pair.Value.Exception);
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
